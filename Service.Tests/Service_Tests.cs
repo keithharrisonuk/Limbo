79f1e06b498e520f8444de41e159da7aac9e591c
Keith <keithharrisonuk@yahoo.co.uk>
2019-03-19 20:55:33 +0000

    init

---
+++ Service.Tests/Service_Tests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using Services;
+
+namespace UnitTests.Services
+{
+    [TestFixture]
+    public class Service_CanMultiply
+    {
+        [Test]
+        public void CanMultiplyNumbers()
+        {
+            Assert.AreEqual(6, new Service().Multiply(2,3));
+        }
+
+        [Test]
+        public void CanMultiplyNumbers1()
+        {
+            Assert.AreEqual(9, new Service().Multiply(3,3));
+        }
+    }
+}
