06ee1dbafede38c4a8e38578ab69d23b2a95a358
Keith <keithharrisonuk@yahoo.co.uk>
2019-03-19 21:10:21 +0000

    working

--- Service.Tests/Service_Tests.cs
+++ Service.Tests/Service_Tests.cs
@@ -6,10 +6,10 @@ namespace UnitTests.Services
     [TestFixture]
     public class Service_CanMultiply
     {
-        [Test]
-        public void CanMultiplyNumbers()
+        [TestCase(2, 3, 6)]
+        public void CanMultiplyNumbers(int a, int b, int expected)
         {
-            Assert.AreEqual(6, new Service().Multiply(2,3));
+            Assert.AreEqual(expected, new Service().Multiply(a, b));
         }
 
         [Test]
@@ -26,7 +26,6 @@ namespace UnitTests.Services
             Assert.AreEqual(expected, new Service().Add(a, b));
         }
         // Oh no!
-
-// asdasd
+        
     }
 }
